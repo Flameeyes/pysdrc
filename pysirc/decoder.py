eb33990364b85e9d56b5b04f9fec84e3b17f07d9
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-05-15 22:01:22 +0100

    Code dump for the basic encoder/decoder functions.

---
+++ pysirc/decoder.py
@@ -0,0 +1,72 @@
+# SPDX-FileCopyrightText: 2020 Diego Elio PettenÃ²
+# SPDX-FileCopyrightText: 2020 Diego Elio PettenÃƒÂ²
+# SPDX-FileCopyrightText: 2020 Diego Elio Pettenò
+#
+# SPDX-License-Identifier: MIT
+
+"""Decoder interfaces for SIRC protocol."""
+
+class DecodeException(Exception):
+    """Raised when a set of pulse timings are not a valid SIRC command."""
+
+def _bits_to_value_lsb(bits):
+    result = 0
+
+    for position, value in enumerate(bits):
+        result += value << position
+
+    return result
+
+
+def decode_sirc(pulses):
+    """Decode SIRC (Sony) protocol commands from raw pulses.
+
+    The Sony command protocol uses a different format than the normal RC-5,
+    which requires a different decoding scheme.
+
+    Details of the protocol can be found at:
+
+    https://www.sbprojects.net/knowledge/ir/sirc.php
+    http://www.righto.com/2010/03/understanding-sony-ir-remote-codes-lirc.html
+    """
+
+    # SIRC supports 12-, 15- and 20-bit commands. There's always one header pulse,
+    # and then two pulses per bit, so accept 25-, 31- and 41-pulses commands.
+    if not len(pulses) in [25, 31, 41]:
+        raise DecodeException("Invalid number of pulses %d" % len(pulses))
+
+    if not 2200 <= pulses[0] <= 2600:
+        raise DecodeException("Invalid header pulse length (%d usec)" % pulses[0])
+
+    evens = pulses[1::2]
+    odds = pulses[2::2]
+    pairs = zip(evens, odds)
+    bits = []
+    for even, odd in pairs:
+        if odd > even * 1.75:
+            bits.append(1)
+        else:
+            bits.append(0)
+
+    command_bits = bits[0:7]
+
+    # 20-bit commands are the same as 12-bit but with an additional 8-bit
+    # extension. 15-bit commands use 8-bit for the device address instead.
+    if len(pulses) == 31:
+        device_bits = bits[7:15]
+        extended_bits = None
+    else:
+        device_bits = bits[7:12]
+        extended_bits = bits[12:]
+
+    command = _bits_to_value_lsb(command_bits)
+    device = _bits_to_value_lsb(device_bits)
+    if extended_bits:
+        extended = _bits_to_value_lsb(extended_bits)
+    else:
+        extended = None
+
+    if extended is None:
+        return (command, device)
+
+    return (command, device, extended)
---
+++ pysirc/encoder.py
@@ -0,0 +1,43 @@
+# SPDX-FileCopyrightText: 2020 Diego Elio Pettenò
+#
+# SPDX-License-Identifier: MIT
+
+"""Encoder for SIRC protocol."""
+
+class EncodeError(ValueError):
+    """Raised when the provided value is not valid for the protocol."""
+
+def _value_to_pulses(value, bits):
+    pulses = []
+    for _ in range(bits):
+        if value & 1:
+            pulses.extend((600, 1200))
+        else:
+            pulses.extend((600, 600))
+
+        value = value >> 1
+
+    return pulses
+
+def encode_sirc(command, device, extended_device=None):
+    if command >= 2**12:
+        raise EncodeError('Invalid command %x' % command)
+    if extended_device:
+        if extended_device >= 2**8:
+            raise EncodeError("Invalid extended device %x" % extended_device)
+        if device >= 2**5:
+            raise EncodeError("Invalid device %x" % device)
+    else:
+        if device >= 2**8:
+            raise EncodeError('Invalid device ID %x' % device)
+
+    pulses = [2400]
+    pulses.extend(_value_to_pulses(command, 7))
+    if device >= 2**5:
+        pulses.extend(_value_to_pulses(device, 8))
+    else:
+        pulses.extend(_value_to_pulses(device, 5))
+    if extended_device:
+        pulses.extend(_value_to_pulses(extended_device, 8))
+    
+    return pulses

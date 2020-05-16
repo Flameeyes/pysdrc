fc780588e6da86cc2277fe2f9609f94fea1b8f59
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-05-16 15:14:20 +0100

    Add basic setup configuration.
    Note that the package is configured pretty much entirely in `setup.cfg` only.

---
+++ setup.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2002 Diego Elio Pettenò
+#
+# SPDX-License-Identifier: Unlicense
+
+from setuptools import setup
+
+# Ensure it's present.
+import setuptools_scm  # noqa: F401
+
+setup()

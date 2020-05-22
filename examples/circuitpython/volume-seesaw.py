03f91b3840e4a0609b5c05841496bb860b5b8a24
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-05-22 13:00:35 +0100

    Add a module to wrap around SIRC transmission for CircuitPython.
    Also included an example that turns volume up and down repeatedly.

---
+++ examples/circuitpython/volume-seesaw.py
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: 2020 Diego Elio Pettenò
+#
+# SPDX-License-Identifier: MIT
+
+"""Example CircuitPython code that raises/lowers volume continuously.
+
+The example expects the following files in the CIRCUITPY volume:
+
+ - /sirc_transmitter.py
+ - /pysirc/encoder.py
+
+It also assumes the correct IR LED to be connected to the D5 line, with HIGH
+corresponding to ON (common cathode or via transistor).
+
+"""
+
+import time
+
+import board
+
+import sirc_transmitter
+
+transmitter = sirc_transmitter.SircTransmitter(board.D5)
+
+while True:
+    print("Volume++")
+    transmitter.transmit_command(18, 1)
+    time.sleep(2)
+
+    print("Volume--")
+    transmitter.transmit_command(19, 1)
+    time.sleep(2)

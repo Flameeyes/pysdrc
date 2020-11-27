9a43929d6348605f7807b8ccf14fb83ace14c255
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-11-27 18:17:04 +0000

    Add repeat as parameter.
    Instead of always repeating every pulse four times, set it as a parameter, and allow per-transmission overrides.

--- pysirc/circuitpython/transmitter.py
+++ pysirc/circuitpython/transmitter.py
@@ -16,19 +16,29 @@ from pysirc import encoder
 class Transmitter:
     """Generic transmitter for infrared remotes."""
 
-    def __init__(self, pin, carrier_frequency=38_000, duty_cycle=2 ** 15):
+    def __init__(
+        self, pin, carrier_frequency=38_000, duty_cycle=2 ** 15, default_repeat=1
+    ):
         self._pwm = pulseio.PWMOut(
             pin, frequency=carrier_frequency, duty_cycle=duty_cycle
         )
         self._pulseout = pulseio.PulseOut(self._pwm)
+        self._default_repeat = default_repeat
+        if default_repeat < 1:
+            raise ValueError(
+                "default_repeat should be at least 1, got %d" % default_repeat
+            )
 
-    def transmit_pulses(self, pulses):
+    def transmit_pulses(self, pulses, repeat=None):
         """Transmit a set of pre-calculated pulses."""
+
+        if not repeat:
+            repeat = self._default_repeat
 
         if not isinstance(pulses, array.array):
             pulses = array.array("H", pulses)
 
-        for _ in range(4):
+        for _ in range(repeat):
             self._pulseout.send(pulses)
             time.sleep(0.025)
 
@@ -36,22 +46,27 @@ class Transmitter:
 class SIRCTransmitter(Transmitter):
     """Transmitter for Sony SIRC remote protocol."""
 
-    def __init__(self, pin, carrier_frequency=40_000, duty_cycle=2 ** 15):
+    def __init__(
+        self, pin, carrier_frequency=40_000, duty_cycle=2 ** 15, default_repeat=4
+    ):
         super().__init__(
-            pin, carrier_frequency=carrier_frequency, duty_cycle=duty_cycle
+            pin,
+            carrier_frequency=carrier_frequency,
+            duty_cycle=duty_cycle,
+            default_repeat=default_repeat,
         )
 
-    def transmit_command(self, command, device, extended_device=None):
+    def transmit_command(self, command, device, extended_device=None, repeat=None):
         pulses = encoder.encode_sirc(command, device, extended_device)
-        self.transmit_pulses(pulses)
+        self.transmit_pulses(pulses, repeat=repeat)
 
 
 class NECTransmitter(Transmitter):
     """Transmitter for NEC remote protocol."""
 
-    def transmit_command(self, address, command):
+    def transmit_command(self, address, command, repeat=None):
         pulses = encoder.encode_nec(address, command)
-        self.transmit_pulses(pulses)
+        self.transmit_pulses(pulses, repeat=repeat)
 
-    def transmit_repeat(self):
-        self.transmit_pulses(encoder.NEC_REPEAT)
+    def transmit_repeat(self, repeat=None):
+        self.transmit_pulses(encoder.NEC_REPEAT, repeat=repeat)
